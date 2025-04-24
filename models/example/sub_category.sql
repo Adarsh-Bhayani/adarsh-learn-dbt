cbac0d3f363d0b4a83f5c66f3d9ed52f327b279e
Adarsh Bhayani <adarsh.bhayani@simformsolutions.com>
2025-04-24 12:52:40 +0000

    edit in sub category file

--- models/example/sub_category.sql
+++ models/example/sub_category.sql
@@ -1,3 +1,12 @@
-select category ,sub_category,sum(profit) as total_profit
+
+{{ config(materialized = 'table')}}
+
+select 
+ country ,
+ state ,
+ count(order_id) as total_order,
+ count(customer_id) as total_customer,
+ sum(sales) as total_sales,
+ sum(profit) as total_profit
 from orders
-group by category , sub_category
+group by country , state 
